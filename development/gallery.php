a7f8423e16048f02f0beb35b516a4206bccc5790
Rosemary <perkinsrosemaryl@gmail.com>
2015-06-04 23:16:02 -0700

    Testing using images as large thumbnails

--- development/gallery.php
+++ development/gallery.php
@@ -9,22 +9,26 @@ require_once "includes/header.inc.php";
 
 <script>
 	$(document).ready(function() {
-		$(".full-img").colorbox({rel:'group1'});
+		$(".full-img").colorbox({rel:'group3', transition:"fade", width:"75%"});
+		$(".caption").colorbox({rel:'caption', transition:"fade", height: "100%"})
 	})
 </script>
 
 	<section class="gallery">
-		<a href="images/truck.jpg" class="full-img">Photo 1</a>
-		<a href="http://lorempixel.com/230/150/abstract" class="full-img">Photo 2</a>
-		<a href="http://lorempixel.com/230/150/abstract" class="full-img">Photo 3</a>
+		<a href="images/truck.jpg" title="This is Isobel's truck on the side of a steep hill" class="full-img"><img src="images/truck.jpg" alt=""></a>
+		<a href="images/truck2.jpg" title="Here's the other side of the road.  Also steep." class="full-img"></a>
+		<a href="images/truck3.jpg" title="The truck looks cute looking out into the trees and hills" class="full-img"></a>
 		<h3>
-			Album 1
+			Truck Pictures
 		</h3>
 	</section>
 	<section class="gallery">
-		<img src="http://lorempixel.com/230/150/abstract" alt="Placeholder image" class="full-img">
+		<a href="images/hiking1.jpg" title="Flowers along NF 6808" class="caption group2"><img src="images/hiking1.jpg" alt=""></a>
+		<a href="images/hiking2.jpg" title="Mount Adams hiding behind the hills" class="caption group2"></a>
+		<a href="images/hiking3.jpg" title="Wildflowers on Grassy Knoll" class="caption group2"></a>
+
 		<h3>
-			Album 2
+			Hiking Pictures
 		</h3>
 	</section>
 	<section class="gallery">
