180b937252ee7414f20df18b8e65706e5889f469
Rosemary <perkinsrosemaryl@gmail.com>
2015-06-11 23:24:37 -0700

    Fixed styling of footer nav

--- development/includes/footer.inc.php
+++ development/includes/footer.inc.php
@@ -3,7 +3,18 @@
 </div>
 
 <footer>
-	<?php include ("includes/nav.inc.php"); ?>
+	<nav>
+		<ul>
+			<li><a href="national.php"><span>National</span></a></li>
+			<li><a href="officers.php"><span>Officers</span></a></li>
+			<li><a href="youth.php"><span>Youth</span></a></li>
+			<li><a href="partners.php"><span>Partners</span></a></li>
+			<li><a href="events.php"><span>Events</span></a></li>
+			<li><a href="gallery.php"><span>Gallery</span></a>
+			</li>
+
+		</ul>
+	</nav>
 </footer>
 
 </body>
