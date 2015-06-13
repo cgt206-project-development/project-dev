d73edcc6ccabde48ed8c4c0ee5811fd9e202ffa3
Rosemary <perkinsrosemaryl@gmail.com>
2015-06-12 18:21:47 -0700

    Added content to index page

--- development/index.php
+++ development/index.php
@@ -7,15 +7,15 @@ require_once "includes/header.inc.php";
 	<div class="three-col-container">
 		<section class="three-col">
 			<h3>Mission</h3>
-			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium doloremque reprehenderit nulla quia numquam explicabo natus illum eveniet, cumque nostrum iure quibusdam iusto placeat esse libero, consectetur eius consequuntur deserunt.
+			The Mission of the League of United Latin American Citizens is to advance the economic condition, educational attainment, political influence, housing, health and civil rights of the Hispanic population of the United States.
 		</section>
 		<section class="three-col">
 			<h3>Community Involvement</h3>
-			<img src="http://lorempixel.com/220/160/abstract" alt="Placeholder image" class="full-img">
+			<img src="images/index_images/community.jpg" alt="Southwest Washington LULAC showing support of justice and dignity for all US immigrants." class="full-img">
 		</section>
 		<section class="three-col">
-			<h3>Partners</h3>
-			<img src="http://lorempixel.com/220/160/abstract" alt="Placeholder image" class="full-img">
+			<h3><a href="partners.php">Partners</a></h3>
+			<img src="images/index_images/partners.jpg" alt="Picnic with Vancouver Police." class="full-img">
 		</section>
 	</div>
 	<div class="clearFix"></div>
@@ -23,7 +23,15 @@ require_once "includes/header.inc.php";
 	<div class="thirty-sixty-container">
 		<section class="thirty">
 			<h3>History</h3>
-			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus quo magni doloremque libero et quisquam, aliquam, sapiente sint modi soluta expedita placeat fugiat minus repellat architecto iure atque eius voluptatum.
+			<p>
+				The League of United Latin American Citizens has a long history. It begins with the annexation of a third of Mexico’s territory following the Mexican War, which resulted in nearly 77,000 Mexicans becoming United States citizens. For generations, these citizens were to be plagued by prejudice that would result in overt acts of discrimination and segregation. This prejudice led to the curtailment of many civil rights.
+			</p>
+			<p>
+				In Texas, prejudice and acts of discrimination had reached such extreme proportions that Mexican Americans began organizing to defend themselves. There were three main organizations: The Order of the Sons of America, the Knights of America in San Antonio, and The League of Latin American Citizens.  All three organizations worked hard to merge and form the League of United Latin American Citizens (LULAC) in 1929. Today the League of United Latin American Citizens is the oldest and most widely respected Hispanic civil rights organization in the United States. The founders of LULAC created an organization that empowers its members to create and develop opportunities where they are needed most. Learn more about the history of LULAC here: http://lulac.org/about/history/
+			</p>
+			<p>
+				Annually, LULAC engages its network of 135,000 community volunteers, 1,000 local councils, 60 community technology centers and 14 LULAC National Educational Service Centers to empower Latino families through direct service programs and advocacy in 35 states, the District of Columbia and Puerto Rico. Southwest Washington LULAC Council 47013, founded in August 2011 in Vancouver, Wa, is one of 1,000 local councils made up of volunteer community leaders. As a council in good standing, we have access to other councils, National programs, technology centers, and LULAC National Educational Service Centers.
+			</p>
 		</section>
 		<section class="sixty"><iframe width="100%" height="315" src="https://www.youtube.com/embed/ZreyAAYqvPE" frameborder="0" allowfullscreen></iframe></section>
 	</div>
