bff63497b87e10f58d65960e880eab0be4bdc4a4
Rosemary <perkinsrosemaryl@gmail.com>
2015-06-12 21:09:21 -0700

    Finished adding content and most styling to index.php

--- development/index.php
+++ development/index.php
@@ -23,21 +23,21 @@ require_once "includes/header.inc.php";
 	<div class="fifty-fifty-container">
 		<section class="sixty">
 			<h3>LULAC in the Pacific Northwest</h3>
-			LULAC is growing in the Pacific Northwest where the changing demographics call for grassroots organization in order to effectively address local, state, and regional issues. Through the organization of a formal LULAC council community leaders come together to use the great power that LULAC yields at the National level in delivering the mission of advancing the economic condition, educational attainment, political influence, housing, health and civil rights of the Latino population at the local level. 
+			LULAC is growing in the Pacific Northwest where the changing demographics call for grassroots organization in order to effectively address local, state, and regional issues. Through the organization of a formal LULAC council community leaders come together to use the great power that LULAC yields at the National level in delivering the mission of advancing the economic condition, educational attainment, political influence, housing, health and civil rights of the Latino population at the local level.
 		</section>
 		<section class="thirty">
 			<h3>Southwest WA LULAC Council 47013</h3>
 			<p>
-				Southwest WA LULAC Council out of Vancouver, WA and LULAC of Lane County out of Eugene, OR have been working together to deliver the mission of LULAC in the Pacific Northwest.  If you are interested in formalizing a LULAC Council in your area please visit http://lulac.org/members/ for instructions and contact Diana Perez at roseprez13@gmail.com.  For more information on LULAC of Lane County, OR contact jcvalle@lulaclanecounty.org.
+				Southwest WA LULAC Council out of Vancouver, WA and LULAC of Lane County out of Eugene, OR have been working together to deliver the mission of LULAC in the Pacific Northwest.  If you are interested in formalizing a LULAC Council in your area please visit <a href="http://lulac.org/members/">LULAC Members</a> for instructions and contact Diana Perez at roseprez13@gmail.com.  For more information on LULAC of Lane County, OR contact jcvalle@lulaclanecounty.org.
 			</p>
 		</section>
 	</div>
 	<div class="clearFix"></div>
-	<h3>For information on attending a Southwest WA LULAC Council meeting contact southwestwashingtonlulac@gmail.com</h3>
+	<h3 class="meeting-info">
+		For information on attending a Southwest WA LULAC Council meeting contact southwestwashingtonlulac@gmail.com
+	</h3>
 	<div class="fifty-fifty-container">
-<!-- 		<section class="thirty">
-			For information on attending a Southwest WA LULAC Council meeting contact southwestwashingtonlulac@gmail.com
-		</section> -->
+
 		<section>
 			<h3>Standing committees in Southwest WA LULAC Council</h3>
 			<ul>
@@ -60,9 +60,6 @@ require_once "includes/header.inc.php";
 		</section>
 	</div>
 
-	<!-- <div class="clearFix"></div> -->
-
-	<!-- <div class="thirty-sixty-container"> -->
 	<div>
 		<section class="fifty-fifty-container">
 			<h3>History</h3>
@@ -73,7 +70,7 @@ require_once "includes/header.inc.php";
 				In Texas, prejudice and acts of discrimination had reached such extreme proportions that Mexican Americans began organizing to defend themselves. There were three main organizations: The Order of the Sons of America, the Knights of America in San Antonio, and The League of Latin American Citizens.  All three organizations worked hard to merge and form the League of United Latin American Citizens (LULAC) in 1929. 
 
 			<p class="fifty-fifty">
-				Today the League of United Latin American Citizens is the oldest and most widely respected Hispanic civil rights organization in the United States. The founders of LULAC created an organization that empowers its members to create and develop opportunities where they are needed most. Learn more about the history of LULAC here: http://lulac.org/about/history/
+				Today the League of United Latin American Citizens is the oldest and most widely respected Hispanic civil rights organization in the United States. The founders of LULAC created an organization that empowers its members to create and develop opportunities where they are needed most. <a href="http://lulac.org/about/history/">Learn more</a> about the history of LULAC.
 			</p>
 			<p class="fifty-fifty">
 				Annually, LULAC engages its network of 135,000 community volunteers, 1,000 local councils, 60 community technology centers and 14 LULAC National Educational Service Centers to empower Latino families through direct service programs and advocacy in 35 states, the District of Columbia and Puerto Rico. Southwest Washington LULAC Council 47013, founded in August 2011 in Vancouver, Wa, is one of 1,000 local councils made up of volunteer community leaders. As a council in good standing, we have access to other councils, National programs, technology centers, and LULAC National Educational Service Centers.
