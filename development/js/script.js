424233931effca3e97341ade01d0d5a0edc06e22
Rosemary <perkinsrosemaryl@gmail.com>
2015-06-12 21:27:34 -0700

    Cleared cache

---
+++ development/js/script.js
@@ -0,0 +1,28 @@
+( function( $ ) {
+$( document ).ready(function() {
+$('.menu > ul').prepend('<li class=\"mobile\"><a href=\"#\"><span>Menu <i>&#9776;</i></span></a></li>');
+$('.menu > ul > li > a').click(function(e) {
+  $('.menu li').removeClass('active');
+  $(this).closest('li').addClass('active');	
+  var checkElement = $(this).next();
+  if((checkElement.is('ul')) && (checkElement.is(':visible'))) {
+    $(this).closest('li').removeClass('active');
+    checkElement.slideUp('normal');
+  }
+  if((checkElement.is('ul')) && (!checkElement.is(':visible'))) {
+    $('.menu ul ul:visible').slideUp('normal');
+    checkElement.slideDown('normal');
+  }
+  if( $(this).parent().hasClass('mobile') ) {
+    e.preventDefault();
+    $('.menu').toggleClass('expand');
+  }
+  if($(this).closest('li').find('ul').children().length == 0) {
+    return true;
+  } else {
+    return false;	
+  }		
+});
+
+});
+} )( jQuery );
