cc10324788105c56f02a00dc175ca3e14c84e355
Rosemary <perkinsrosemaryl@gmail.com>
2015-06-12 18:03:22 -0700

    Added updated list of partners and descriptions

--- development/partners.php
+++ development/partners.php
@@ -4,15 +4,35 @@ $page_heading = "Community Partners";
 require_once "includes/header.inc.php";
  ?>
 	<ul class="partners">
-		<li><a href="#">City Of Vancouver</a></li>
-		<li><a href="#">Clark College</a></li>
-		<li><a href="#">Gifford-Pinchot National Forest</a></li>
-		<li><a href="#">Hope Heart Institute</a></li>
-		<li><a href="#">Latino Civic Alliance</a></li>
-		<li><a href="#">NAACP Vancouver</a></li>
-		<li><a href="#">National LULAC</a></li>
-		<li><a href="#">One America</a></li>
-		<li><a href="#">SeaMar</a></li>
-		<li><a href="#">WA Commission on Hispanic Affairs</a></li>
+		<li><a href="http://www.hmccoregon.com/">Hispanic Metropolitan Chamber</a>
+			<ul>
+				The Hispanic Metropolitan Chamber works with all members of the community to increase the economic advancement of Latinos in Oregon and SW Washington. Southwest WA LULAC Council is a partner with the Hispanic Chamber because we want to build a better understanding of Hispanic business contributions, increase availability of technical assistance, and provide scholarship opportunities to Latino youth in the region.
+			</ul>
+		</li>
+		<li><a href="http://www.clarkcountylatinoyouthconference.com/">Clark County Latino Youth Conference</a>
+			<ul>
+				The mission of Clark County Latino Youth Conference is to encourage and motivate Latino youth to pursue a higher education or technical career, seek opportunities, and engage in positive behaviors.  Clark County Latino Youth Conference is a 501 (c)(3) registered charitable organization in Washington State and provides invaluable resources to support our future leaders. An annual youth conference is held every year in early October.
+			</ul>
+		</li>
+		<li><a href="https://www.saltmoney.org/index.html">SALT</a>
+			<ul>
+				SALT is a free educational program that helps every student who wants a college degree to get it in a financially responsible way. Through our partnership with SALT, students in high school and college/university are able to access a database of available scholarships, receive financial literacy materials, and learn how to master their money so they become financially secure.
+			</ul>
+		</li>
+		<li><a href="http://www.nwcave.org/">National Women's Coalition Against Violence and Exploitation (NWCAVE)</a>
+			<ul>
+				NWCAVE has a DREAM and so do we...a World Without Violence &amp; Exploitation.  NWCAVE is a volunteer operated national 501(c)(3) nonprofit organization located in the pacific northwest. Our Council works with NWCAVE to inform, educate and prevent violence and exploitation against women and children in our local communities by providing workshops and training.
+			</ul>
+		</li>
+		<li><a href="http://www.cityofvancouver.us/police">City of Vancouver Police Department</a>
+			<ul>
+				The Vancouver Police Department serves the citizens of Vancouver with Valor, Professionalism and Duty.  Our Council has a special relationship with the City of Vancouver Police Department through active participation on the Chief's Diversity Advisory Team and a formal agreement on working together to build community trust within the Latino community. We continue to work towards building healthier communities and address Police Community Relationships.
+			</ul>
+		</li>
+		<li><a href="https://www.facebook.com/Clark.LCRG">Latino Community Resource Group</a>
+			<ul>
+				The Latino Community Resource Group is a coalition of individuals and organizations that promote the success and empowerment of the Latinos.  Every year an annual Resource Fair brings together about 300 community members to share and promote health and resource information.  Our Council is a proud supporter of the annual Spring Resource Fair.
+			</ul>
+		</li>
 	</ul>
  <?php require_once "includes/footer.inc.php"; ?>
