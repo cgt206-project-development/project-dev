6255b96a5366c903d73dc76c92ac4648c5af94f6
Rosemary <perkinsrosemaryl@gmail.com>
2015-06-12 18:48:53 -0700

    Add content to youth page

--- development/youth.php
+++ development/youth.php
@@ -5,15 +5,15 @@ require_once "includes/header.inc.php";
  ?>
 	<section class="youth">
 		<h3>
-			Heading 3
+			¡Adelante! America Youth Leadership Program
 		</h3>
 		<p>
-			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea ducimus sunt, debitis sit facere recusandae ratione, harum amet autem aliquid quisquam neque, aspernatur laudantium minima dolore, cumque fugit incidunt alias? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta alias, eaque labore consectetur fugit odit, sint, rem amet et sapiente eos omnis quasi nemo tempore hic fugiat. Possimus voluptatem, assumenda!
+			This youth leadership development program is a collaborative effort between LULAC and AT&amp;T for underserved youth to encourage participants to conduct community service projects, meet with positive role models and explore alternatives to violence and crime. The program works with Latino youth to cultivate optimism, build resilience, and improve academic skills with the goal of successful high school completion. Mentoring programs are crucial to help motivate youth that reside in underrepresented communities. Currently, Fort Vancouver High School in Vancouver, WA is the first site in the Pacific Northwest to have this youth leadership program during the school year.  For more information contact our program coordinator Jennifer Cazares at southwestwashingtonlulac@gmail.com.
 		</p>
 
 		<div class="thirty-sixty-container">
-			<img src="http://lorempixel.com/220/220/abstract" alt="Placeholder image" class="full-img">
-			<img src="http://lorempixel.com/460/230/abstract" alt="Placeholder image" class="full-img">
+			<img src="images/youth_images/3.jpg" alt="Placeholder image" class="full-img">
+			<img src="images/youth_images/2.jpg" alt="Placeholder image" class="full-img">
 
 		</div>
 	</section>
@@ -21,17 +21,17 @@ require_once "includes/header.inc.php";
 
 	<section class="youth">
 		<h3>
-			Heading 3
+			LULAC Youth Council 20000
 		</h3>
 
 		<p>
-			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae culpa expedita iusto vitae nobis deleniti eum, impedit natus asperiores, a, commodi vero enim assumenda velit eos corporis debitis doloremque veritatis. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam quaerat eaque unde vitae, quos provident obcaecati iure, eius voluptates? Dolorem, porro? Dolore quas magnam sint distinctio culpa, reiciendis doloremque ad.
+			The LULAC Youth is an entity designed to carry forth the work of LULAC at the teenage level and to serve as a training base for its members through social, civic, sports, and parliamentary activities. The focus of the LULAC Youth program is the development of young people in bettering themselves and their community through positive educational experiences. LULAC Youth members shall be supported by responsible LULAC adult members who have demonstrated an interest in the well being of our youth.  Youth Council 20000 was chartered in 2014 at Mountain View High School in Vancouver, WA.  For more information contact Amy Ruger at Amy.Ruger@evergreenps.org.
 		</p>
+	</section>
+	<section class="youth">
+		<h3>Scholarship Opportunities</h3>
 		<p>
-			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto odit tempora corporis modi tempore unde, cum commodi, qui sunt harum. Odit voluptatem repellendus nulla voluptatibus asperiores corporis labore, sint autem. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis quisquam cumque repellat eum voluptates dolore, totam deserunt quaerat tenetur harum possimus nemo nobis sunt molestias, pariatur sint distinctio a consequatur.
-		</p>
-		<p>
-			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum praesentium placeat corrupti esse deserunt fugiat. Culpa optio magni in temporibus fugit esse, perferendis ipsam, ab sit ducimus, dolor atque minus. Lorem ipsum dolor sit amet, consectetur adipisicing elit. In alias, quam facere. Sit maiores, quibusdam tenetur aliquam, veritatis officia, voluptatum quia veniam, doloremque sunt consequuntur mollitia dolor laborum culpa. Accusantium.
+			Through our partnership with the Hispanic Metropolitan Chamber of Commerce we are able to grow opportunities for student scholarships. Each dollar we raise for scholarships is matched 100% by the Hispanic Metropolitan Chamber!  Several thousands of dollars have already been given to students in southwest Washington. If you are interested in donating funds for student scholarships, please contact Diana Avalos-Leos at Clark.co.latinoyouthconference@gmail.com.
 		</p>
 	</section>
  <?php require_once "includes/footer.inc.php"; ?>
